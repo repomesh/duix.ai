1d20346c5165871cb22d30f0bf480bffea1d3387
songwei <songwei@btows.com>
2026-03-25 17:09:01 +0800

    update

--- duix-ios/GJLocalDigitalDemo/GJLocalDigitalDemo/ViewController.mm
+++ duix-ios/GJLocalDigitalDemo/GJLocalDigitalDemo/ViewController.mm
@@ -30,8 +30,7 @@
 //基础模型 git 地址下载较慢，请下载后自己管理加速
 #define BASEMODELURL   @"https://github.com/GuijiAI/duix.ai/releases/download/v1.0.0/gj_dh_res.zip"
 //数字人模型 git 地址下载较慢，请下载后自己管理加速
-#define DIGITALMODELURL @"https://github.com/GuijiAI/duix.ai/releases/download/v1.0.0/shuziren_v2.zip"
-
+#define DIGITALMODELURL @"https://github.com/GuijiAI/duix.ai/releases/download/v1.0.0/bendi3_20240518.zip"
 
 
 @interface ViewController ()<GJDownWavToolDelegate,UITextViewDelegate>
